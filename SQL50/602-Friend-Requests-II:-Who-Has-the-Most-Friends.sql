d8927f0b3a10adc453e69078cc14553761d15f7d
Maramallah <157317456+Maramallah@users.noreply.github.com>
2025-01-08 23:40:57 +0200

    [602] [Time Beats: 31.00%] - LeetPush

--- SQL50/602-Friend-Requests-II:-Who-Has-the-Most-Friends.sql
+++ SQL50/602-Friend-Requests-II:-Who-Has-the-Most-Friends.sql
@@ -1,18 +1,16 @@
 # Write your MySQL query statement below
--- cte table that has all ides superetly 
-WITH
-  AllIds AS (
-    SELECT requester_id AS id FROM RequestAccepted
-    UNION ALL
-    SELECT accepter_id FROM RequestAccepted
-  )
-SELECT
-  id,
-  -- count all even null 
-  COUNT(*) AS num
-FROM AllIds
--- means group by first column
-GROUP BY 1
--- order by second 
-ORDER BY 2 DESC
-LIMIT 1;
+-- use union once to get how many he accepted and how many he requested 
+-- union all cuz there will be duplications
+-- order by sum count 
+-- get the most by limit 1
+select id , sum(c) as num from (
+select requester_id as id , count(requester_id) as c 
+from RequestAccepted 
+group by requester_id
+union all
+select accepter_id as id , count(accepter_id) as c 
+from RequestAccepted 
+group by accepter_id) s
+group by id 
+order by sum(c) desc
+limit 1
