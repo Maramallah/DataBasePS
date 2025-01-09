acd28b770dd20b33ba4d17992b0abc70f7c643d4
Maramallah <157317456+Maramallah@users.noreply.github.com>
2025-01-10 00:04:41 +0200

    [1321] [Time Beats: 22.42%] - LeetPush

--- SQL50/1321-Restaurant-Growth.sql
+++ SQL50/1321-Restaurant-Growth.sql
@@ -16,22 +16,31 @@
 -- -- here cuz i don`t want to the first 6 days so off set delets them 
 -- limit 100000000000 offset 6;
 -- or use a cte and start date with 6 days after first day
-WITH RollingWindow AS (
-    SELECT
-        visited_on,
-        SUM(amount) OVER (
-            ORDER BY visited_on
-            RANGE BETWEEN INTERVAL 6 DAY PRECEDING AND CURRENT ROW
-        ) AS amount,
-        ROUND(SUM(amount) OVER (
-            ORDER BY visited_on
-            RANGE BETWEEN INTERVAL 6 DAY PRECEDING AND CURRENT ROW
-        ) / 7, 2) AS average_amount,
-        -- this over do the same job as (select min(visited_on) from customers)
-        MIN(visited_on) OVER () + INTERVAL 6 DAY AS start_date
-    FROM Customer
-)
-SELECT distinct(visited_on), amount, average_amount
-FROM RollingWindow
-WHERE visited_on >= start_date
-ORDER BY visited_on;
+-- WITH RollingWindow AS (
+--     SELECT
+--         visited_on,
+--         SUM(amount) OVER (
+--             ORDER BY visited_on
+--             RANGE BETWEEN INTERVAL 6 DAY PRECEDING AND CURRENT ROW
+--         ) AS amount,
+--         ROUND(SUM(amount) OVER (
+--             ORDER BY visited_on
+--             RANGE BETWEEN INTERVAL 6 DAY PRECEDING AND CURRENT ROW
+--         ) / 7, 2) AS average_amount,
+--         -- this over do the same job as (select min(visited_on) from customers)
+--         MIN(visited_on) OVER () + INTERVAL 6 DAY AS start_date
+--     FROM Customer
+-- )
+-- SELECT distinct(visited_on), amount, average_amount
+-- FROM RollingWindow
+-- WHERE visited_on >= start_date
+-- ORDER BY visited_on;
+-- or use sekf join 
+select a.visited_on , sum(amount) as amount ,
+round(sum(amount)/7,2) as average_amount
+from (select distinct(visited_on) from Customer ) a join Customer b
+on datediff(a.visited_on , b.visited_on ) between 0 and 6 
+group by  a.visited_on
+having count(distinct(b.visited_on)) =7
+order by a.visited_on 
+
